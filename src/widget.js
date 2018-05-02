ad28f4587dd9318b4f5b4adeb217744ae2a03d35
milko1 <4094708+milko1@users.noreply.github.com>
2018-05-02 13:00:01 -0400

    added config option to exclude bots

--- src/widget.js
+++ src/widget.js
@@ -161,7 +161,20 @@
       }
     }
 
-    // BEGIN: Milko: country code filtering
+    // BEGIN: Milko
+    // BEGIN: bot detection & avoidance
+    if (_bftn_options.bots_exclude) {
+      _bftn_util.log("bots_exclude='" + _bftn_options.bots_exclude + "'");
+      var BOTS_UA = _bftn_options.bots_ua || "(Pingdom\.com\_bot|AdsBot\-|googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
+      var re = new RegExp(BOTS_UA, 'i');
+      if (bots_found = re.exec(window.navigator.userAgent)) {
+        _bftn_util.log("bots detected!=", bots_found);
+        return;
+      }
+    }
+    // END: bot detection & avoidance
+    
+    // BEGIN: country code filtering
     var init2 = function () {
       // Only show once per day.
       _bftn_util.setCookie('_BFTN_WIDGET_VIEW', 'true', _bftn_options.viewCookieExpires);
@@ -179,10 +192,10 @@
       var _BFTN_WIDGET_COUNTRY_CODE = '_BFTN_WIDGET_COUNTRY_CODE';
       _bftn_options_country_code_show = (_bftn_options.country_code_show.constructor === Array ? _bftn_options.country_code_show : [_bftn_options.country_code_show]);
 
-      var _country_code_cache = _bftn_util.getCookie(_BFTN_WIDGET_COUNTRY_CODE);
-      if (_country_code_cache && _country_code_cache.length > 0) {  // if country cached already
-        _bftn_util.log("_country_code_cache='" + _country_code_cache + "'");
-        if (_bftn_options_country_code_show.indexOf(_country_code_cache) > -1)  // if country cached is in show array
+      var country_code_cache = _bftn_util.getCookie(_BFTN_WIDGET_COUNTRY_CODE);
+      if (country_code_cache && country_code_cache.length > 0) {  // if country cached already
+        _bftn_util.log("_country_code_cache='" + country_code_cache + "'");
+        if (_bftn_options_country_code_show.indexOf(country_code_cache) > -1)  // if country cached is in show array
           init2();
 
         return;
